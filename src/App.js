ae2074411c7a580f19c3ba9a1dde8a0018af2f4d
Adrian Nelson <anelson@andigital.com>
2018-07-29 16:12:50 +0100

    Added Axios and a notification for deleted messages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Ideas from './components/Ideas';
 import NewIdeaButton from './components/NewIdeaButton';
+import Notification from './components/Notification';
+import axios from 'axios';
 import './App.css';
 
 const generateRandomNumber = () => Math.floor(Math.random() * Math.floor(10000000));
@@ -19,12 +21,15 @@ class App extends Component {
     this.state = {
       cards: [{...defaultCard}],
       isFocus: false,
-      focusId: 0
+      focusId: 0,
+      notificationMessage: '',
+      isNotificationOpen: false
     };
     this.updateTitle = this.updateTitle.bind(this);
     this.updateBodyText = this.updateBodyText.bind(this);
     this.addIdea = this.addIdea.bind(this);
     this.deleteIdea = this.deleteIdea.bind(this);
+    this.closeNotification = this.closeNotification.bind(this);
   }
 
   updateTitle(title, id) {
@@ -55,11 +60,30 @@ class App extends Component {
   }
 
   deleteIdea(id) {
-    this.setState((prevState) => {
-      const index = prevState.cards.findIndex((element) =>  element.id === Number(id));
-      prevState.cards.splice(index, 1);
-      return prevState;
-    });
+    // axios.post(`https://ideaboard.com/idea/delete/${id}`)
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        // if (res.status === 202) {
+        this.setState((prevState) => {
+          console.log('res1: ', res)
+          const index = prevState.cards.findIndex((element) =>  element.id === Number(id));
+          prevState.cards.splice(index, 1);
+          prevState.notificationMessage = 'Note deleted successfully';
+          prevState.isNotificationOpen = true;
+          return prevState;
+        })
+      })
+      .catch(() => {
+        this.setState((prevState) => {
+          prevState.notificationMessage = 'Error something has gone wrong';
+          prevState.isNotificationOpen = true;
+          return prevState;
+        })
+      })
+  }
+
+  closeNotification(title, id) {
+    this.setState({ isNotificationOpen: false });
   }
 
   render() {
@@ -75,6 +99,11 @@ class App extends Component {
           updateBodyText={this.updateBodyText}
         />
         <NewIdeaButton addIdea={this.addIdea}/>
+        <Notification
+          message={this.state.notificationMessage}
+          isOpen={this.state.isNotificationOpen}
+          close={this.closeNotification}
+        />
       </div>
     );
   }
---
+++ src/components/Notification/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+const styles = theme => ({
+  close: {
+    width: theme.spacing.unit * 4,
+    height: theme.spacing.unit * 4,
+  },
+});
+
+class SimpleSnackbar extends React.Component {
+  state = {
+    open: false,
+  };
+
+  componentDidUpdate(prevProps) {
+    // Typical usage (don't forget to compare props):
+    if (this.props.isOpen && !prevProps.isOpen) {
+      this.handleClick()
+    }
+  }
+
+  handleClick = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ open: false });
+    this.props.close();
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={this.state.open}
+          autoHideDuration={5000}
+          onClose={this.handleClose}
+          ContentProps={{
+            'aria-describedby': 'message-id',
+          }}
+          message={<span id="message-id">{this.props.message}</span>}
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              className={classes.close}
+              onClick={this.handleClose}
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
+      </div>
+    );
+  }
+}
+
+SimpleSnackbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  message: PropTypes.string.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  close: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(SimpleSnackbar);
